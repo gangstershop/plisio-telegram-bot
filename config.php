aa297d551ce9cf229cb3dce0a46546baad05f70f
gangstershop <gsaxeli969@gmail.com>
2024-11-03 21:39:29 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,2 +1,5 @@
 <?php
 const API_KEY = 'plisio_secret_key';
+
+define('TELEGRAM_BOT_TOKEN', '7530642477:AAGE15ZpKwqV_IgVH_eBDyGmbJn-XX-f1RQ');
+define('PLISIO_API_KEY', 'vx90H1XReSxUtE6AJRbXZwkYjhXn3FfljOqwmqqsSHTi-E6pf49unEouV70ZjLxX');
